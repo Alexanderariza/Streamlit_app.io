4268ca5d8a9782ad87e8fdac40dbf38be40e319c
Alexander Ariza <66517893+Alexanderariza@users.noreply.github.com>
2022-02-15 00:39:56 +0100

    Update basemaps.py

--- basemaps.py
+++ basemaps.py
@@ -1,20 +1,22 @@
-import streamlit as st
+# Import libraries
+import ee
 import geemap.foliumap as geemap
 
-
-def app():
-    st.title("Change basemaps")
-
-    keys = list(geemap.ee_basemaps.keys())[1:]
-
-    basemap = st.selectbox("Select a basemap", keys)
-
-    code = f"""import geemap
-m = geemap.Map()
-m.add_basemap('{basemap}')
-m"""
-    st.code(code)
-
-    m = geemap.Map()
-    m.add_basemap(basemap)
-    m.to_streamlit()
+# Create an interactive map
+Map = geemap.Map(plugin_Draw=True, Draw_export=False)
+# Add a basemap
+Map.add_basemap("TERRAIN")
+# Retrieve Earth Engine dataset
+dem = ee.Image("USGS/SRTMGL1_003")
+# Set visualization parameters
+vis_params = {
+    "min": 0,
+    "max": 4000,
+    "palette": ["006633", "E5FFCC", "662A00", "D8D8D8", "F5F5F5"],
+}
+# Add the Earth Engine image to the map
+Map.addLayer(dem, vis_params, "SRTM DEM", True, 0.5)
+# Add a colorbar to the map
+Map.add_colorbar(vis_params["palette"], 0, 4000, caption="Elevation (m)")
+# Render the map using streamlit
+Map.to_streamlit()
